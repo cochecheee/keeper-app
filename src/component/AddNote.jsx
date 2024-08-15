1dc9092c471e1e578f395e2192d50dc18ca7086f
cochecheee <buitien747@gmail.com>
2024-08-16 00:53:21 +0700

    finish fontend of keeper app

--- src/component/AddNote.jsx
+++ src/component/AddNote.jsx
@@ -1,18 +1,34 @@
 import React from "react"
+import AddIcon from '@mui/icons-material/Add';
+import Fab from '@mui/material/Fab';
+import Zoom from '@mui/material/Zoom';
 
 function AddNote(props) {
-    return <div>
-        <form>
-            <input onChange={(event) => {
+    const [isExpanded, setExpanded] = React.useState(false)
+
+    function expandInputArea() {
+        setExpanded(true)
+    }
+
+    return <div >
+        <form className="create-note">
+            {isExpanded && <input onChange={(event) => {
                 props.handleChange(event)
-            }} name="title" placeholder="Title" />
-            <textarea onChange={(event) => {
-                props.handleChange(event)
-            }} name="content" placeholder="Take a note..." rows="3" />
+            }} name="title" placeholder="Title" />}
+
+            <textarea
+                onChange={(event) => {
+                    props.handleChange(event)
+                }}
+                onClick={() => expandInputArea()}
+                name="content" placeholder="Take a note..." rows={isExpanded ? 3 : 1} />
             {/* Pass the new note back to the App. */}
-            <button onClick={(event) => {
-                props.addNote(event)
-            }}>Add</button>
+
+            <Zoom in={isExpanded}>
+                <Fab onClick={(event) => {
+                    props.addNote(event)
+                }}><AddIcon /></Fab>
+            </Zoom>
         </form>
     </div>
 }
